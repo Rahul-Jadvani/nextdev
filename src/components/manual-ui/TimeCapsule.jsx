c89aeb8f9fa0474a89b5c1ce45bc1da2eb84a151
Rahul Jadvani <rahuljadvani24@gmail.com>
2025-06-27 12:14:35 +0530

    test commit

--- src/components/manual-ui/TimeCapsule.jsx
+++ src/components/manual-ui/TimeCapsule.jsx
@@ -6,37 +6,37 @@ const InfiniteMenu = dynamic(
 
 const items = [
   {
-    image: '/image/img1.jpg',
+    image: '/Image/img1.jpg',
     link: 'https://photos.google.com/share/AF1QipNEGYc5FDJn0ZCoWH-qxTzVWG4JPwnGQTZttEenHl7IvpK1BZk_MMN0dBy_xudtJg?key=T29DMzcxamNYeW5JV3ExVmJyTk9GcXdnNkVXNWNR',
     title: 'DSU DEVHACK 2024 TIME CAMPSULE',
     description: 'A look into Last year\'s hackaton'
   },
   {
-    image: '/image/img2.jpg',
+    image: '/Image/img2.jpg',
     link: 'https://photos.google.com/share/AF1QipNEGYc5FDJn0ZCoWH-qxTzVWG4JPwnGQTZttEenHl7IvpK1BZk_MMN0dBy_xudtJg?key=T29DMzcxamNYeW5JV3ExVmJyTk9GcXdnNkVXNWNR',
     title: 'First DSU DevHack',
     description: 'The inaugural hackathon that started it all.'
   },
   {
-    image: '/image/img3.jpg',
+    image: '/Image/img3.jpg',
     link: 'https://photos.google.com/share/AF1QipNEGYc5FDJn0ZCoWH-qxTzVWG4JPwnGQTZttEenHl7IvpK1BZk_MMN0dBy_xudtJg?key=T29DMzcxamNYeW5JV3ExVmJyTk9GcXdnNkVXNWNR',
     title: 'Strong Reach',
     description: 'Attracted 2000+ Registrations and 300+ Onground Hackers'
   },
   {
-    image: '/image/img4.jpg',
+    image: '/Image/img4.jpg',
     link: 'https://photos.google.com/share/AF1QipNEGYc5FDJn0ZCoWH-qxTzVWG4JPwnGQTZttEenHl7IvpK1BZk_MMN0dBy_xudtJg?key=T29DMzcxamNYeW5JV3ExVmJyTk9GcXdnNkVXNWNR',
     title: 'Hybrid Format',
     description: '36 Hours of energetic coding'
     },
     {
-    image: '/image/img5.jpg',
+    image: '/Image/img5.jpg',
     link: 'https://photos.google.com/share/AF1QipNEGYc5FDJn0ZCoWH-qxTzVWG4JPwnGQTZttEenHl7IvpK1BZk_MMN0dBy_xudtJg?key=T29DMzcxamNYeW5JV3ExVmJyTk9GcXdnNkVXNWNR',
     title: 'Multiple Domains',
     description: '6 different domains including IOT & Web3 for all varieties of hackers'
     },
     {
-    image: '/image/img6.jpg',
+    image: '/Image/img6.jpg',
     link: 'https://photos.google.com/share/AF1QipNEGYc5FDJn0ZCoWH-qxTzVWG4JPwnGQTZttEenHl7IvpK1BZk_MMN0dBy_xudtJg?key=T29DMzcxamNYeW5JV3ExVmJyTk9GcXdnNkVXNWNR',
     title: 'Industrial Experts as Jury',
     description: 'Exceptional Judgement Rounds by experts from Top Industries'
